8002675796a4bfcdec9c870b475661503fb923b7
Eric Lee <lee.eric.03@gmail.com>
2024-10-30 17:35:10 -0600

    adds Prismic client to footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,20 @@
-import "./globals.css"
-import { DM_Sans } from 'next/font/google'
-import { PrismicPreview } from "@prismicio/next";
-import { repositoryName } from "@/prismicio";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import './globals.css';
+import { DM_Sans } from 'next/font/google';
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName } from '@/prismicio';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 const dmSans = DM_Sans({
   subsets: ['latin'],
-  display: 'swap',  
-  variable: '--font-dm-sans',
-})
+  display: 'swap',
+  variable: '--font-dm-sans'
+});
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={dmSans.variable}>
-      <body className="">
+      <body className="bg-[#070815] text-xl text-white">
         <Header />
         <main>{children}</main>
         <Footer />
