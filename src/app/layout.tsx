56431a47eb7307f72bd0de4f6aa23397c00c8edc
Eric Lee <lee.eric.03@gmail.com>
2024-10-30 16:16:02 -0600

    adds tailwind, Prettier for tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
+import "./globals.css"
+import { DM_Sans } from 'next/font/google'
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  display: 'swap',  
+  variable: '--font-dm-sans',
+})
 
 export default function RootLayout({
   children,
@@ -7,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={dmSans.variable}>
+      <body className="">{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
